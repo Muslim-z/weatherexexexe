017eae3569230c5d3dd494259261b1b9df456c72
Muslim-z <83671712+Muslim-z@users.noreply.github.com>
2021-12-15 19:37:29 +0300

    weatherxexexe

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import requests
 
-
-KING_URL = 'https://wttr.in/'
-
-
 if __name__ == '__main__':
-    url1 = f'{KING_URL}london&?m&?M&lang=ru'
-    response1 = requests.get(url1)
-    response1.raise_for_status()
-    print(response1.text)
-
-    url2 = f'{KING_URL}svo&?m&?M&lang=ru'
-    response2 = requests.get(url2)
-    response2.raise_for_status()
-    print(response2.text)
-
-    url3 = f'{KING_URL}Череповец&?m&?M&lang=ru'
-    response3 = requests.get(url3)
-    response3.raise_for_status()
-    print(response3.text)
+    payload = {
+        "m": "",
+        "M": "",
+        "lang": "ru"
+    }
+    cities = [
+        "london",
+        "svo",
+        "Череповец"
+    ]
+    for city in cities:
+        response = requests.get(f'https://wttr.in/{city}', params=payload)
+        response.raise_for_status()
+        print(response.text)
